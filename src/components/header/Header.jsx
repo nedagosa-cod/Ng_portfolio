8cd9bd2b5f14eb3b2a7bfd2d22e96e6ba87b3033
nedagosa-cod <nestorcode4004@gmail.com>
2023-05-16 22:25:12 -0500

    config navbar and header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import './style.css'
+import React, { useEffect, useState } from 'react'
+import './style.scss'
 import cartoonMe from '../../img/headerMe.png'
 
+import Marquee from 'react-fast-marquee'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 const Header = () => {
+
+  const [scrolled, setScrolled] = useState('left')
+
+
   return (
     <header className='header' data-scroll-section>
-        <article>
-            <h1>Nestor David Gomez</h1>
-            <h2>Frontend Developer</h2>
+        <article data-scroll data-scroll-speed="2">
+            <h2>Welcome!</h2>
+            <h3>I'm a Frontend Developer</h3>
+            <p>I hope you enjoy the experience with my portfolio, I did my best to make it a different personal Web Site. <br />Follow me on my social media accounts.</p>
+            <div>
+              <FontAwesomeIcon icon={faLinkedin} />
+              <FontAwesomeIcon icon={faTwitter} />
+              <FontAwesomeIcon icon={faGithub} />
+            </div>
         </article>
-        <section>
+        <section data-scroll data-scroll-speed="-2">
             <figure>
                 <img src={cartoonMe} alt="Foto de perfil version cartoon" />
             </figure>
         </section>
+        <Marquee 
+          className='Marquee'
+          direction={scrolled}
+        ><h1>Néstor David Gómez -</h1></Marquee>
     </header>
   )
 }
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import './style.css'
+import logo from '../../img/logo.png'
+import './style.scss'
 
 export const NavBar = () => {
   return (
     <nav className='navbar' data-scroll-section>
-        <ul>
-            <li><a href="Home">Home</a></li>
-            <li><a href="About">About</a></li>
-            <li><a href="Skills">Skills</a></li>
-            <li><a href="Projects">Projects</a></li>
-            <li><a href="Contact">Contact</a></li>
-        </ul>
+      <span>{'<NG>'}</span>
+      <ul>
+          <li><a href="Home">Home</a></li>
+          <li><a href="About">About</a></li>
+          <li><a href="Skills">Skills</a></li>
+          <li><a href="Projects">Projects</a></li>
+          <li><a href="Contact">Contact</a></li>
+      </ul>
     </nav>
   )
 }
